a8aa3985362a897369a40e754e06e00860a7698a
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-05-02 14:43:22 +0900

    add button for introduce and tech stack

---
+++ app.js
@@ -0,0 +1,12 @@
+var content1 = document.querySelector(".content1");
+var content2 = document.querySelector(".content2");
+
+function showContent1() {
+	content1.style.display = "block";
+	content2.style.display = "none";
+}
+
+function showContent2() {
+	content1.style.display = "none";
+	content2.style.display = "block";
+}
