db8e72460e76a9de1b6c8408deb5cd098c4e8f4e
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-05-05 17:26:07 +0900

    add video and introduce content

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var content1 = document.querySelector(".content1");
 var content2 = document.querySelector(".content2");
 
 function showContent1() {
-	content1.style.display = "block";
-	content2.style.display = "none";
+  content1.style.display = "block";
+  content2.style.display = "none";
 }
 
 function showContent2() {
-	content1.style.display = "none";
-	content2.style.display = "block";
+  content1.style.display = "none";
+  content2.style.display = "block";
 }
