69a7b544df34ca1c77b4f2a39bf6d2fa76088d3e
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-05-05 22:25:24 +0900

    modify page and add music

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var content1 = document.querySelector(".content1");
 var content2 = document.querySelector(".content2");
 
 function showContent1() {
-  content1.style.display = "block";
+  content1.style.display = "flex";
   content2.style.display = "none";
 }
 
